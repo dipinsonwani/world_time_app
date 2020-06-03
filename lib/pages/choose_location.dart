fe3394fade417c8225cb24063b91cee4849eddc2
dipinsonwani <55944433+dipinsonwani@users.noreply.github.com>
2020-06-03 23:34:47 +0530

    Initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Choose a location'),
+        centerTitle: true,
+      ),
+
+    );
+  }
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getData() async {
+
+    Response response =await get('https://jsonplaceholder.typicode.com/todos/1');
+    print(response.body);
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('loading screen'),
+    );
+  }
+}
